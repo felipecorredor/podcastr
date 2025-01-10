92c3e099513da445d6dd5fa8e23c3ba797fd5acc
Felipe Corredor <pipecorredor06@gmail.com>
2025-01-10 13:39:03 -0500

    Added LeftSidebar and RightSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,29 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import { RightSidebar } from "@/components/RightSidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
-        <p className="">LEFT SIDEBAR</p>
-        {children}
-        <p className="">RIGHT SIDEBAR</p>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+        <section>
+          <div>
+            <div>
+              <Image />
+              Mobile Nav
+            </div>
+            <div>
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+
+        <RightSidebar />
       </main>
     </div>
   );
---
+++ type.d.ts
@@ -0,0 +1,17 @@
+declare type SidebarLinks = {
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+declare type VoiceDetails = {
+  id: number;
+  name: string;
+};
+
+declare type PodcastData = {
+  id: number;
+  title: string;
+  description: string;
+  imgURL: string;
+};
